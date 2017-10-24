b63528f21014c9be161ddbacf4673b050b8cc091
luckily248 <luck248@163.com>
2017-10-24 14:10:25 -0400

    add

--- HRIS/main.cpp
+++ HRIS/main.cpp
@@ -8,8 +8,11 @@
 
 #include <iostream>
 #include <sstream>
+#include <vector>
+#include <cstring>
 using namespace std;
 int curId=1;
+
 
 
 class Employee {
@@ -20,7 +23,7 @@ protected:
     string DateofHire;
     int Salary;
     string Title;
-    int Status;//1.regular 2.probation 3.terminal
+    int Status;//1.regular 2.probation 3.terminal 4.contract
 public:
     Employee();
     void setEmployee(string,int,string,string,int,string,int);
@@ -69,8 +72,12 @@ void Regular::changeStatus(int curStatus){
 class Contractor : public Employee{
 
 };
+
+void changeStatusM(vector<Employee> emps);
+
 int main(int argc, const char * argv[]) {
-    int c1;
+    int c;
+    vector<Employee> emps;
     do{
     cout<<"Welcome to CS13 HRIS\n";
     cout<<"-----------------------\n";
@@ -80,18 +87,54 @@ int main(int argc, const char * argv[]) {
     cout<<"3.Add an Employee Record\n";
     cout<<"4.quit\n";
     cout<<"\n";
-    cin>>c1;
+    cin>>c;
     
-        if (c1==1) {
+        if (c==1) {
+            for (vector<Employee>::iterator it=emps.begin(); it!=emps.end(); it++) {
+                Employee emp=*it;
+                cout<<emp.printEmployee();
+            }
+        }else if (c==2){
             
-        }else if (c1==2){
-            
-        }else if (c1==3){
+        }else if (c==3){
             
         }
         
-    }while (c1!=4);
+    }while (c!=4);
     
     return 0;
 }
 
+void changeStatusM(vector<Employee> emps){
+    int id;
+    cout<<"Which Employee you want to Update\n Id=\n";
+    cin>>id;
+    Regular* emp=dynamic_cast<Regular*>(&(emps.at(id-1));
+    if (emp==0) {
+        cout<<"This employee's status can't be changed\n";
+        return;
+    }
+    
+    int c2;
+    do{
+        cout<<"Update Employee Records\n";
+        cout<<"------------------------\n";
+        cout<<"1.Change Salary\n";
+        cout<<"2.Change Title\n";
+        cout<<"3.Change Status\n";
+        cout<<"4.Quit\n";
+        cin>>c2;
+        
+        if (c2==1) {
+            int salary;
+            cout<<"How much Salary\n";
+            cin>>salary;
+        }else if (c2==2){
+            
+        }else if (c2==3){
+            
+        }
+        
+        
+    }while (c2!=4);
+}
